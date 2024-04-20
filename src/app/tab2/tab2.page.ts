8ca2416e034890811a2225ec35101d709f4436ef
Juan Bohigues <jbohigues@vidal-ac.es>
2024-04-20 15:19:40 +0200

    feat: :tada: playing with ionic tutorial

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,49 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  IonFab,
+  IonFabButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+import { PhotoService } from '../services/photo.service';
+import { addIcons } from 'ionicons';
+import { camera } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    IonIcon,
+    IonFab,
+    IonFabButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  ],
 })
 export class Tab2Page {
+  constructor(public photoService: PhotoService) {
+    addIcons({ camera });
+  }
 
-  constructor() {}
-
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
